e0fb76ea768734cec73fcdc682789fa0b608c4b0
Maitê Feld <102314647+m4ite@users.noreply.github.com>
2023-06-15 16:07:31 -0300

    Removing comments

--- led_by_app.ino
+++ led_by_app.ino
@@ -4,14 +4,14 @@
 #include <DabbleESP32.h>
 unsigned long lasttime=0;
 void setup() {
-  Serial.begin(115200);     // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin("MyEsp32");     //set bluetooth name of your device
+  Serial.begin(115200);
+  Dabble.begin("MyEsp32");
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  Dabble.processInput();    //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
-  Serial.print("State:");          //0 if led is Off. 1 if led is On.
+  Dabble.processInput();
+  Serial.print("State:");
   Serial.print(LedControl.getpinState());
   digitalWrite(LED, LedControl.getpinState());
   }
